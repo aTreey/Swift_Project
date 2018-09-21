fd12048fc80f67c72da49797d41fd9476178b964
yuhongpeng <480814177@qq.com>
2018-09-21 11:28:55 +0800

    初始化swift项目

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Swift_Project' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Swift_Project
+
+  target 'Swift_ProjectTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Swift_ProjectUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Swift_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Project
+//
+//  Created by HongpengYu on 2018/9/21.
+//  Copyright © 2018 HongpengYu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
